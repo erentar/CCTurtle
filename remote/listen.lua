cf2ede1de95c74ff4f84ecbabc96aebcf7b988ee
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2020-10-10 16:37:02 -0400

    Update listen.lua
    greatly lowers the amount of lines

--- remote/listen.lua
+++ remote/listen.lua
@@ -4,27 +4,16 @@ modemPer.open(0)
 
 while true do
 	eventType,peripheralSide,recieveChannel,replyChannel,message = os.pullEvent("modem_message")
-	if message == "forward" then
-		turtle.forward()
-	elseif message == "back" then
-		turtle.back()
-	elseif message == "left" then
-		turtle.turnLeft()
-	elseif message == "right" then
-		turtle.turnRight()
-	elseif message == "up" then
-		turtle.up()
-	elseif message == "down" then
-		turtle.down()
-	elseif message == "mine" then
-		turtle.dig()
-	elseif message == "mineUp" then
-		turtle.digUp()
-	elseif message == "mineDown" then
-		turtle.digDown()
-	elseif message == "exit" then
-		break
-	end
+	if message == "forward" then turtle.forward()
+	elseif message == "back" then turtle.back()
+	elseif message == "left" then turtle.turnLeft()
+	elseif message == "right" then turtle.turnRight()
+	elseif message == "up" then turtle.up()
+	elseif message == "down" then turtle.down()
+	elseif message == "mine" then turtle.dig()
+	elseif message == "mineUp" then turtle.digUp()
+	elseif message == "mineDown" then turtle.digDown()
+	elseif message == "exit" then break end
 end
 
 modemPer.closeAll() --close previously open channels
