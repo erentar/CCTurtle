05752f98a0060ed24a8961669a771b5d6faf23f8
erentar <erentar2002@gmail.com>
2026-06-27 21:15:52 +0200

    sort2

--- sort.lua
+++ sort.lua
@@ -1,100 +1,141 @@
-local tArgs = { ... }
-local chest = peripheral.wrap(tArgs[1])
-local buffer = peripheral.wrap(tArgs[2])
+--++ definitions
+local alphabet = {
+    a = 11,
+    b = 12,
+    c = 13,
+    d = 14,
+    e = 15,
+    f = 16,
+    g = 17,
+    h = 18,
+    i = 19,
+    j = 20,
+    k = 21,
+    l = 22,
+    m = 23,
+    n = 24,
+    o = 25,
+    p = 26,
+    q = 27,
+    r = 28,
+    s = 29,
+    t = 30,
+    u = 31,
+    v = 32,
+    w = 33,
+    x = 34,
+    y = 35,
+    z = 36,
+    _ = 37,
+    [":"] = 38
+}
 
-local chestList = chest.list()
-local chestSpan = 0 -- this variable keeps track of what slot the last item in the chest is, useful when removing spaces
+local function characterOrder(char1,char2) -- returns true if char1 comes before char2 on the alphabet
+    return alphabet[char1] < alphabet[char2]
+end
 
-
--- remove empty space
-local sorted = false
-while not sorted do
-
-    chestList = chest.list()
-
-    chestSpan = 0
-    for slot,item in pairs(chestList) do
-        if slot > chestSpan then chestSpan = slot end
+local function stringOrder(str1, str2) -- returns true if str1 comes before str2 on the dictionary
+    local shorter
+    if #str1 < #str2 then
+        shorter = str1
+    else
+        shorter = str2
     end
-
-    sorted = true
-    for slot = 2,chestSpan do
-        if chestList[slot-1] == nil then
-            sorted = false
-            chest.pushItems(peripheral.getName(chest), slot, 999, slot-1)
+    for i=1,#shorter do
+        local char1 = str1:sub(i,i)
+        local char2 = str2:sub(i,i)
+        if char1 ~= char2 then
+            return characterOrder(char1,char2)
         end
+    end
+    -- if not returned yet, all characters are equal, however strings may differ in length
+    if shorter == str1 then
+        return true
+    else
+        return false
     end
 end
 
--- sort
-function swapItems(a,b)
+local function move(inventoryName,from,to)
+    inventory.pushItems(inventoryName,from,9999,to)
+end
+
+local function swap(inventoryName,a,b)
     parallel.waitForAll(
-        function() chest.pushItems(peripheral.getName(buffer),a,999,1) end,
-        function() chest.pushItems(peripheral.getName(buffer),b,999,2) end,
-        function() chest.pullItems(peripheral.getName(buffer),2,999,a) end,
-        function() chest.pullItems(peripheral.getName(buffer),1,999,b) end
+        function() move(inventoryName,a,chestSize) end,
+        function() move(inventoryName,b,a) end,
+        function() move(inventoryName,chestSize,b) end
     )
 end
 
-function comesBeforeAlphabetical(a,b) -- says if a precedes b in alphabetical order
-    local orderOfChars = {
-        a = 1,
-        b = 2,
-        c = 3,
-        d = 4,
-        e = 5,
-        f = 6,
-        g = 7,
-        h = 8,
-        i = 9,
-        j = 10,
-        k = 11,
-        l = 12,
-        m = 13,
-        n = 14,
-        o = 15,
-        p = 16,
-        q = 17,
-        r = 18,
-        s = 19,
-        t = 20,
-        u = 21,
-        v = 22,
-        w = 23,
-        x = 24,
-        y = 25,
-        z = 26,
-        _ = 27,
-        [":"] = 28
-    }
-    local shorter
-    if #a < #b then
-        shorter = a
-    else
-        shorter = b
+---- definitions
+
+local startTime = os.epoch("utc")
+
+local chests = { peripheral.find("inventory") } -- what the fuck is this
+for _, chest1 in pairs(chests) do
+    local chest = chest1
+    local chestList = chest.list()
+    chestSize = chest.size()
+    local chestName = peripheral.getName(chest)
+
+    local emptySlots = {}
+    for i=1,chestSize do
+        if chestList[i] == nil then
+            table.insert(emptySlots,i)
+            break
+        end
     end
-    for i=1, #shorter do
-        if a:sub(i,i) == b:sub(i,i) then
-            do end
-        elseif orderOfChars[a:sub(i,i)] < orderOfChars[b:sub(i,i)] then
-            -- a is first
-            return true
-        elseif orderOfChars[a:sub(i,i)] > orderOfChars[b:sub(i,i)] then
-            -- b is first
-            return false
+
+    if #emptySlots == 0 then
+        error("chest needs one empty slot")
+    end
+
+    local lastEmptySlot = emptySlots[#emptySlots]
+    if lastEmptySlot ~= chestSize then -- last slot is not empty
+        move(chestName,chestSize,emptySlots[1])
+    end
+
+    local sorted = false
+    while sorted == false do
+        sorted = true
+        for i=2,chestSize do
+            if chestList[i] == nil then
+                goto continue
+            end
+            -- here on not nil
+
+            if chestList[i-1] == nil then
+                sorted = false
+                parallel.waitForAll(
+                    function() move(chestName,i,i-1) end,
+                    function() chestList = chest.list() end
+                )
+                i = i-1
+            end
+
+            if (chestList[i-1] == nil) then
+                goto continue
+            end
+
+            if not stringOrder(chestList[i-1]["name"],chestList[i]["name"]) then
+                parallel.waitForAll(
+                    function() swap(chest,i-1,i) end,
+                    function() chestList = chest.list() end
+                )
+                sorted = false
+            elseif chestList[i-1]["name"] == chestList[i]["name"] then
+                parallel.waitForAll(
+                    function() move(chestName,i,i-1) end,
+                    function() chestList = chest.list() end
+                )
+                sorted = false
+            end
+            ::continue::
         end
     end
 end
 
-sorted = 0
-local itemsHaveBeenSwapped = true
-while itemsHaveBeenSwapped do
-    chestList = chest.list()
-    itemsHaveBeenSwapped = false
-    for slot=2,chestSpan do
-        if comesBeforeAlphabetical(chestList[slot].name,chestList[slot-1].name) then
-            itemsHaveBeenSwapped = true
-            swapItems(slot,slot-1)
-        end
-    end
-end
+local endTime = os.epoch("utc")
+timing = endTime - startTime
+print(timing)
